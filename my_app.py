27056799428bf4de26a7c72584cfa7a985ac4749
aryanmaheshwari1420 <98485902+aryanmaheshwari1420@users.noreply.github.com>
2023-07-26 23:54:41 +0530

    done with the UI part

--- my_app.py
+++ my_app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request
 # Load the trained model from the pickle file
 import pickle
 
-with open('wine_quality.pkl', 'rb') as file:
+with open('random_forest_model.pkl', 'rb') as file:
     model = pickle.load(file)
 
 # Create a Flask app
@@ -24,24 +24,20 @@ def predict():
     feature4 = float(request.form['feature4'])
     feature5 = float(request.form['feature5'])
     feature6 = float(request.form['feature6'])
-    feature7 = float(request.form['feature7'])
-    feature8 = float(request.form['feature8'])
-    feature9 = float(request.form['feature9'])
-    feature10 = float(request.form['feature10'])
-    feature11 = float(request.form['feature11'])
+
     # Add more features as needed
 
     # Preprocess the input features
     # Make sure to preprocess the input data the same way you did during model training (e.g., normalization)
 
     # Make the prediction using the loaded model
-    prediction = model.predict([[feature1, feature2, feature3, feature4, feature5, feature6, feature7, feature8, feature9, feature10, feature11]])
+    prediction = model.predict([[feature1, feature2, feature3, feature4, feature5, feature6]])
     
     # Get the predicted class label (0 or 1)
     predicted_label = prediction[0]
 
     # Print the input features and prediction for debugging
-    print("Input features:", [feature1, feature2, feature3, feature4, feature5, feature6, feature7, feature8, feature9, feature10, feature11])
+    print("Input features:", [feature1, feature2, feature3, feature4, feature5, feature6])
     print("Predicted Label:", predicted_label)
 
     # Return the prediction result page
